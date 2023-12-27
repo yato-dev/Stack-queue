43f1e8c49b042812fd9f2125d85d0135ed197936
Shikhar Barthwal <barthwalshikhar@gmail.com>
2023-12-28 03:19:21 +0530

    uc1 updated

---
+++ src/Stack.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Stack {
+    LinkedList<Integer> stackList;
+
+    public Stack() {
+        this.stackList = new LinkedList<>();
+    }
+
+    public void push(int data) {
+        stackList.addFirst(data);
+    }
+
+    public void display() {
+        System.out.println(stackList);
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+
+        
+        stack.display();
+    }
+}
